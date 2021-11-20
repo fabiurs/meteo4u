beb7db0bea0eb4e1d001d6a51b124d1583000f6f
Ursache Fabian <fabian24ursache@gmail.com>
2021-11-20 16:18:03 +0200

    first commit

---
+++ src/UserInputForm/UserInputForm.js
@@ -0,0 +1,17 @@
+
+
+function UserInputForm() {
+    return (
+        <div>
+            <form>
+                <label>
+                    <input
+                        type="text"
+                    />
+                </label>
+            </form>
+        </div>
+    );
+}
+
+export default UserInputForm;
